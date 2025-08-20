41a8595214b1856cb060edc4dadb6ca6bd38c9a5
Vladyslav Tarkovskyi <vladtarkovsky@gmail.com>
2025-08-20 09:29:49 +0300

    added BasePlayer model

---
+++ src/main/java/com/interview/test/models/BasePlayer.java
@@ -0,0 +1,87 @@
+package com.interview.test.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Base Player Model with common fields
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+abstract class BasePlayer {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("login")
+    private String login;
+
+    @JsonProperty("role")
+    private String role;
+
+    @JsonProperty("age")
+    private Integer age;
+
+    @JsonProperty("gender")
+    private String gender;
+
+    @JsonProperty("screenName")
+    private String screenName;
+
+    // Constructors
+    public BasePlayer() {}
+
+    // Getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public Integer getAge() { return age; }
+    public void setAge(Integer age) { this.age = age; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getScreenName() { return screenName; }
+    public void setScreenName(String screenName) { this.screenName = screenName; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BasePlayer that = (BasePlayer) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(login, that.login) &&
+                Objects.equals(role, that.role) &&
+                Objects.equals(age, that.age) &&
+                Objects.equals(gender, that.gender) &&
+                Objects.equals(screenName, that.screenName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, role, age, gender, screenName);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", role='" + role + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", screenName='" + screenName + '\'' +
+                '}';
+    }
+}
